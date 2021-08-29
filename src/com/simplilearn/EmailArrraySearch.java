02efb5cd5e6931ed5c36aafbeeef127df8855aa5
nimishkapsime <nimish.kapsime02@gmail.com>
2021-08-29 11:34:57 +0530

    emailarraycode

---
+++ src/com/simplilearn/EmailArrraySearch.java
@@ -0,0 +1,43 @@
+package com.simplilearn;
+
+import java.util.Scanner;
+
+public class EmailArrraySearch {
+
+	public static void main(String[] args) {
+		String email [] = {"abc@gmail.com","def@gmail.com","ghi@gmail.com"};
+		String esearch;
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter email to search");
+		esearch=sc.next();
+		boolean flag =false;
+		for (int i =0; i <= email.length;i++)
+		{
+			String avalue = email[i];
+
+			if(esearch.equals(avalue))
+			{
+				flag= true;
+				break;
+			}
+
+		}
+		if(flag = true)
+
+		{
+			System.out.println("Email id found in Array: "+esearch);
+		}
+		else 
+		{ 
+			System.out.println("Email id not found in Array: "+esearch);
+		}
+
+
+	}
+
+
+
+
+
+}
